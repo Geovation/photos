78d803563af028a1304b60591893d10aaa0f9ff0
Konstantinos Dalkafoukis <kdalkafoukis@yahoo.gr>
2018-08-16 11:44:33 +0100

    capture a geolocated photo and data #4 (#18)
    * capture a geolocated photo and data #4
    
    * fixing "Cannot read property 'addEventListener' of null" error
    
    * change position of send photo button
    
    * semicolons
    
    * code styling - remove some semicolons

--- src/App.js
+++ src/App.js
@@ -3,42 +3,52 @@ import React, { Component } from 'react';
 import Page1 from './Components/Page1';
 import Page2 from './Components/Page2';
 import Page3 from './Components/Page3';
+import PhotoPage from './Components/PhotoPage';
 import LandingPage from './Components/LandingPage';
 
 class App extends Component {
   constructor(props){
-      super(props)
+      super(props);
       this.state={
         page1:false,
         page2:false,
-        page3:false
+        page3:false,
+        photopage:false,
+        file:null
       }
   }
 
   openPage1 = () =>{
-    this.setState({page1:true})
+    this.setState({page1:true});
   }
 
   closePage1 = () =>{
-    this.setState({page1:false})
+    this.setState({page1:false});
   }
 
   openPage2 = () =>{
-    this.setState({page2:true})
+    this.setState({page2:true});
   }
 
   closePage2 = () =>{
-    this.setState({page2:false})
+    this.setState({page2:false});
   }
 
   openPage3 = () =>{
-    this.setState({page3:true})
+    this.setState({page3:true});
   }
 
   closePage3 = () =>{
-    this.setState({page3:false})
+    this.setState({page3:false});
   }
 
+  openPhotoPage = (file) =>{
+    this.setState({photopage:true,file});
+  }
+
+  closePhotoPage = () =>{
+    this.setState({photopage:false});
+  }
 
   render() {
     return (
@@ -54,12 +64,17 @@ class App extends Component {
                   ?
                   <Page3 closePage={this.closePage3}/>
                   :
+                  this.state.photopage
+                      ?
+                        <PhotoPage file={this.state.file} closePage={this.closePhotoPage}/>
+                      :
                       <LandingPage
                           openMenu={this.openMenu}
                           closeMenu={this.closeMenu}
                           openPage1={this.openPage1}
                           openPage2={this.openPage2}
                           openPage3={this.openPage3}
+                          openPhotoPage={this.openPhotoPage}
                       />
     );
   }
