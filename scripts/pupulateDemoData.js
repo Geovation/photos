90e90fc1d985d8bd375d15549b23ed0c1306eb1b
Sebastian <sebastian.ovide@gmail.com>
2018-11-06 18:58:52 +0000

    upload photos

--- scripts/pupulateDemoData.js
+++ scripts/pupulateDemoData.js
@@ -2,10 +2,9 @@
  * This script just populate the DB with demo data. The ID's are prefixed with "test_" for make it easy to find them and
  * eventually delete them.
  */
-
 const admin = require('firebase-admin');
-
 const randomLocation = require('random-location');
+const Jimp = require('jimp');
 
 const serviceAccount = require('./serviceAccountKey.json');
 const LONDON_COORDS = {
@@ -14,31 +13,61 @@ const LONDON_COORDS = {
 };
 const R = 20 * 1000;
 
-async function addMetaDataSync(id) {
+let image;
+let font;
+const fileNameGeovation = 'geovation.jpg';
+let bucket;
+let db;
 
+async function addMetaDataSync(id) {
   const rndLocation = randomLocation.randomCirclePoint(LONDON_COORDS, R);
   const location = new admin.firestore.GeoPoint(rndLocation.latitude, rndLocation.longitude);
   const data = {
-    updated: FieldValue.serverTimestamp(),
+    updated: admin.firestore.FieldValue.serverTimestamp(),
     location: location,
     description: `${id} some text here`
   };
 
   console.log(`Adding ${id} with data: ${data}`);
 
-  await photosCol.doc(id).set(data).catch(e => {
-    console.error(e);
+  return await db.collection('photos').doc(id).set(data);
+}
+
+async function addPhotoSync(id) {
+  // watermark it with id
+  image.print(font, 0, 0, id).write('tmp.jpg');
+  console.log(`Uploading ${id}`);
+
+  // upload it as "original"
+  return await bucket.upload('tmp.jpg', { destination: `photos/${id}` });
+}
+
+async function run(num) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    storageBucket: "photos-demo-d4b14.appspot.com"
   });
+  bucket = admin.storage().bucket();
+  db = admin.firestore();
+  const settings = {timestampsInSnapshots: true};
+  db.settings(settings);
+
+  image = await Jimp.read(fileNameGeovation);
+  font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
+
+  for (let i=0; i< num; i++) {
+    const id = `test_${i}`;
+    await addMetaDataSync(id);
+    await addPhotoSync(id);
+  }
 }
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-
-const db = admin.firestore();
-const FieldValue = admin.firestore.FieldValue;
-const photosCol = db.collection('photos');
-
-for (let i=0; i< 100; i++) {
-  addMetaDataSync(`test_${i}`);
-}
+run(100)
+  .then(_ => {
+    console.log("END");
+    process.exit(0);
+  })
+  .catch( e => {
+    console.error(e);
+    process.exit(1);
+  });
