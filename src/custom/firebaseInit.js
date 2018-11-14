c2d474c9caadf724464e3b90857a3b8fce86c9a0
Sebastian <sebastian.ovide@gmail.com>
2018-11-14 11:57:16 +0000

    caching databse

--- src/custom/firebaseInit.js
+++ src/custom/firebaseInit.js
@@ -23,4 +23,13 @@ const firestore = firebaseApp.firestore();
 const settings = { timestampsInSnapshots: true };
 firestore.settings(settings);
 
+firebase.firestore().enablePersistence()
+  .catch(function(err) {
+    if (err.code === 'failed-precondition') {
+      console.error("Multiple tabs open, persistence can only be enabled in one tab at a a time.");
+    } else if (err.code === 'unimplemented') {
+      console.error("The current browser does not support all of the features required to enable persistence  ...");
+    }
+  });
+
 export default firebaseApp;
