a3fa29804e3a113a539d9fe88994efca3b898e15
Sebastian Ovide <sebastian.ovide@gmail.com>
2018-11-19 15:46:08 +0000

    fixed geolocation not updating (#114)

--- src/components/PhotoPage.js
+++ src/components/PhotoPage.js
@@ -5,7 +5,9 @@ import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
-import Loading from './Loading';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+// import Loading from './Loading';
 import backButton from '../images/left-arrow.svg';
 import config from '../custom/config';
 import './PhotoPage.scss';
@@ -16,65 +18,61 @@ class PhotoPage extends Component {
     this.state = {
       imgSrc: '',
       open: false,
-      opendialogtext: false,
       message: '',
       value: '',
       sending: false
-    };;
+    };
 
     this.base64 = null;
+    this.dialogCloseCallback = null;
   }
 
   handleChange = (event) => {
     this.setState({ value: event.target.value });
   }
 
-  openDialog = (message) => {
+  openDialog = (message, fn) => {
     this.setState({
       sending: false,
       open: true,
       message
     });
+
+    this.dialogCloseCallback = fn;
   }
 
   closeDialog = () => {
-    this.setState({ open: false });
-    if (this.state.message === 'Failed to upload. Please try again!') {
-      this.loadImage();
-    } else {
-      this.closePage();
-    }
-  }
-
-  openDialogText = () => {
-    this.setState({ opendialogtext: true });
-  }
-
-  closeDialogText = () => {
-    this.setState({ opendialogtext: false });
+    this.dialogCloseCallback ? this.dialogCloseCallback() : this.setState({ open: false });
   }
 
   sendFile = async () => {
-    let data = {}
-    const text =  this.state.value;
-    const { location } = this.props;
-    data.base64 = this.base64;
-    if (text !== '') {
-      data['text'] = text;
-      if (location) {
-        data['latitude'] = location.latitude;
-        data['longitude'] = location.longitude;
-      }
-      this.setState({ sending: true });
-      try {
-        const res = await config.uploadPhoto(data);
-        console.log(res);
-        this.openDialog("Photo was uploaded successfully");
-      } catch (e) {
-        this.openDialog(e.message || e);
-      }
+    if (!this.props.location) {
+      this.openDialog("Could not get the location yet. You won't be able to upload an image.");
     } else {
-      this.openDialogText();
+
+      let data = {};
+      const text =  this.state.value;
+      const { location } = this.props;
+      data.base64 = this.base64;
+      if (text !== '') {
+        data['text'] = text;
+        if (location) {
+          data['latitude'] = location.latitude;
+          data['longitude'] = location.longitude;
+        }
+        this.setState({ sending: true });
+        try {
+          const res = await config.uploadPhoto(data);
+          console.log(res);
+          this.openDialog("Photo was uploaded successfully", this.closePage);
+
+        } catch (e) {
+          this.openDialog(e.message || e);
+        }
+      } else {
+        this.openDialog("Please enter some text");
+      }
+
     }
   }
 
@@ -120,10 +118,7 @@ class PhotoPage extends Component {
 
   render() {
     return (
-      this.state.sending ?
-        <Loading />
-      :
-        <div className='geovation-photos'>
+       <div className='geovation-photos'>
           <div className='headline'>
             <Button onClick={this.closePage}>
               <img className='buttonback' src={backButton} alt=''/>
@@ -157,23 +152,16 @@ class PhotoPage extends Component {
               </Button>
             </DialogActions>
           </Dialog>
-          <Dialog
-            open={this.state.opendialogtext}
-            onClose={this.closeDialogText}
-            aria-labelledby='alert-dialog-title'
-            aria-describedby='alert-dialog-description'
-          >
+
+          <Dialog open={this.state.sending}>
             <DialogContent>
-              <DialogContentText id='alert-dialog-description'>
-                Please enter some text
+              <DialogContentText id="loading-dialog-text">
+                Be patient ;)
               </DialogContentText>
+              <CircularProgress className="progress" size={50} thickness={6}/>
             </DialogContent>
-            <DialogActions>
-              <Button onClick={this.closeDialogText} color='primary'>
-                Ok
-              </Button>
-            </DialogActions>
           </Dialog>
+
         </div>
     );
   }
