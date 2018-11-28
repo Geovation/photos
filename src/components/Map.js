279666e7e63346c0a0e34eaf7751d890f72ec75c
Konstantinos Dalkafoukis <kdalkafoukis@yahoo.gr>
2018-11-28 16:01:37 +0000

    map clustering - solving #132 (#133)
    * map clustering - solving #132

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import _ from "lodash";
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import Button from '@material-ui/core/Button';
@@ -25,11 +26,11 @@ class Map extends Component {
 
   constructor(props) {
     super(props);
-    this.state={
-      openDialog:false,
+    this.state = {
+      openDialog: false,
       feature: {
         properties: {
-          updated:{}
+          updated: {}
         },
         geometry: {
           coordinates:{}
@@ -37,13 +38,13 @@ class Map extends Component {
       }
     }
     this.map = {};
+    this.renderedThumbnails = {};
   }
 
   async componentDidMount(){
     const location = this.props.location;
     const photos = config.dbModule.fetchPhotos();
 
-    mapboxgl.accessToken = ''; // you can add a Mapbox access token here
     this.map = new mapboxgl.Map({
       container: 'map', // container id
       style: 'https://s3-eu-west-1.amazonaws.com/tiles.os.uk/styles/open-zoomstack-outdoor/style.json', //stylesheet location
@@ -54,7 +55,7 @@ class Map extends Component {
 
     this.map.on('load', async () => {
       const geojson = await photos;
-      this.addFeaturesToMap(geojson.features);
+      this.addFeaturesToMap(geojson);
     });
 
     window.gtag('event', 'page_view', {
@@ -63,24 +64,94 @@ class Map extends Component {
     });
   }
 
-  addFeaturesToMap = features => {
-    features.forEach( feature => {
-        // create a DOM element for the marker
-        const el = document.createElement('div');
-        el.className = 'marker';
-        el.style.backgroundImage = `url(${feature.properties.thumbnail}), url(${placeholderImage})`;
+  addFeaturesToMap = geojson => {
+    this.map.addSource("data", {
+        type: "geojson",
+        data: geojson,
+        cluster: true,
+        clusterMaxZoom: 14, // Max zoom to cluster points on
+        clusterRadius: 40 // Radius of each cluster when clustering points (defaults to 50)
+    });
 
-        el.addEventListener('click',()=> {
-            this.setState({
-              openDialog:true,
-              feature
-            })
+    this.map.addLayer({
+        id: "clusters",
+        type: "circle",
+        source: "data",
+        filter: ["has", "point_count"],
+        paint: {
+            // Use step expressions (https://www.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
+            // with three steps to implement three types of circles:
+            //   * Blue, 20px circles when point count is less than 100
+            //   * Yellow, 30px circles when point count is between 100 and 750
+            //   * Pink, 40px circles when point count is greater than or equal to 750
+            "circle-color": [
+                "step",
+                ["get", "point_count"],
+                "#51bbd6",
+                100,
+                "#f1f075",
+                750,
+                "#f28cb1"
+            ],
+            "circle-radius": [
+                "step",
+                ["get", "point_count"],
+                20,
+                100,
+                30,
+                750,
+                40
+            ]
+        }
+    });
+
+    this.map.addLayer({
+        id: "cluster-count",
+        type: "symbol",
+        source: "data",
+        filter: ["has", "point_count"],
+        layout: {
+            "text-field": "{point_count_abbreviated}",
+            "text-font": ["Source Sans Pro Regular"],
+            "text-size": 12
+        }
+    });
+
+    this.map.addLayer({
+        id: "unclustered-point",
+        type: "circle",
+        source: "data",
+        filter: ["!", ["has", "point_count"]],
+        paint: {
+            "circle-color": "#11b4da",
+            "circle-radius": 4,
+            "circle-stroke-width": 1,
+            "circle-stroke-color": "#fff"
+        }
+    });
+
+    this.map.on('render', 'unclustered-point', e => {
+      this.updateRenderedThumbails(e.features);
+    });
+
+    this.map.on('mouseenter', 'clusters', () => {
+        this.map.getCanvas().style.cursor = 'pointer';
+    });
+    this.map.on('mouseleave', 'clusters', () => {
+        this.map.getCanvas().style.cursor = '';
+    });
+
+    this.map.on('click', 'clusters', (e) => {
+      const features = this.map.queryRenderedFeatures(e.point, { layers: ['clusters'] });
+      const clusterId = features[0].properties.cluster_id;
+      this.map.getSource('data').getClusterExpansionZoom(clusterId, (err, zoom) => {
+        if (err)
+            return;
+        this.map.easeTo({
+            center: features[0].geometry.coordinates,
+            zoom: zoom
         });
-
-        // add marker to map
-        new mapboxgl.Marker(el)
-            .setLngLat(feature.geometry.coordinates)
-            .addTo(this.map);
+      });
     });
   }
 
@@ -94,7 +165,40 @@ class Map extends Component {
     this.setState({openDialog:false});
   }
 
+  updateRenderedThumbails = (visibleFeatures) =>{
+    _.forEach(this.renderedThumbnails, (thumbnailUrl, id) => {
+      const exists = !!_.find(visibleFeatures, (feature) => feature.properties.id === id);
+      // if it !exist => remove marker object - delete key from dictionary
+      if (!exists) {
+        this.renderedThumbnails[id].remove();
+        delete this.renderedThumbnails[id];
+      }
+    })
+
+    visibleFeatures.forEach(feature => {
+      if (!this.renderedThumbnails[feature.properties.id]) {
+        //create a div element - give attributes
+        const el = document.createElement('div');
+        el.className = 'marker';
+        el.id = feature.properties.id;
+        el.style.backgroundImage = `url(${feature.properties.thumbnail})`;
+        el.addEventListener('click', () => this.setState({openDialog:true,feature}));
+        //create marker
+        const marker = new mapboxgl.Marker(el)
+          .setLngLat(feature.geometry.coordinates)
+          .addTo(this.map);
+        //save the marker object to the renderedThumbnails dictionary
+        this.renderedThumbnails[feature.properties.id] = marker;
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.map.remove();
+  }
+
   render() {
+
     const feature = this.state.feature;
     const gpsOffline = !(this.props.location.online);
     const gpsDisabled = !this.props.location.updated;
