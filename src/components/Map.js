8c59e9cc4d8324a3d07ce6cc7e95df6b106b5442
Konstantinos <kdalkafoukis@yahoo.gr>
2018-12-03 18:04:37 +0000

    extend the map to the rest of the world - v0.1

--- src/components/Map.js
+++ src/components/Map.js
@@ -18,9 +18,12 @@ import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import { OSstyle } from '../os-style/style';
+import * as turf from '@turf/turf';
+
 
 const CENTER = [-0.07, 51.58];
-const ZOOM = 10;
+const ZOOM = 5;
 
 class Map extends Component {
 
@@ -45,15 +48,17 @@ class Map extends Component {
     const location = this.props.location;
     const photos = config.dbModule.fetchPhotos();
 
+    mapboxgl.accessToken='pk.eyJ1Ijoic2ViYXN0aWFub3ZpZGVnZW92YXRpb251ayIsImEiOiJjanA4ZWwwbTkxdDNxM2twZTgyMGdqOXB5In0.MrWFt3rABCo7n7MBbVRaNw'
     this.map = new mapboxgl.Map({
       container: 'map', // container id
-      style: 'https://s3-eu-west-1.amazonaws.com/tiles.os.uk/styles/open-zoomstack-outdoor/style.json', //stylesheet location
+      style: 'mapbox://styles/mapbox/streets-v10',
       center: location.updated ? [location.longitude, location.latitude] : CENTER, // starting position [lng, lat]
       zoom: ZOOM, // starting zoom
       customAttribution: 'Contains OS data &copy; Crown copyright and database rights 2018'
     });
 
     this.map.on('load', async () => {
+      this.addOSlayers();
       const geojson = await photos;
       this.addFeaturesToMap(geojson);
     });
@@ -62,6 +67,49 @@ class Map extends Component {
       'event_category': 'view',
       'event_label': 'Map'
     });
+  }
+
+  addOSlayers = () =>{
+    // sources have been renamed to composite2 in order to avoid confilct
+    // with mapbox's source id composite
+    this.map.addSource('composite2',OSstyle.sources.composite2);
+    OSstyle.layers.forEach(layer=>{
+      this.map.addLayer(layer);
+    });
+  }
+
+  currentViewContainsBounds = () => {
+    const bbox = [-11.91,49.3,3.61,61.61];  //current bbox
+    const bboxPolygon = turf.bboxPolygon(bbox);
+
+    const bounds = this.map.getBounds();  // bounds of current view
+    const currentBounds = [bounds._ne.lng,bounds._ne.lat,bounds._sw.lng,bounds._sw.lat];
+    const boundsPolygon = turf.bboxPolygon(currentBounds);
+
+    const contains = turf.booleanContains(bboxPolygon, boundsPolygon);
+    return contains;
+  }
+
+  changeVisibility = (visibility) => {
+    const layers = this.map.getStyle().layers;
+    layers.forEach(layer => {
+      if (layer.source === 'composite' && layer.id !== 'water'){
+        this.map.setLayoutProperty(layer.id, 'visibility',visibility);
+      }
+    });
+  }
+
+  updateLayerVisibility = () => {
+    const isContained = this.currentViewContainsBounds();
+    // check one layer if its visible or not
+    const visibility = this.map.getLayoutProperty(this.map.getStyle().layers[1].id, 'visibility');
+
+    if(isContained && visibility==='visible'){
+      this.changeVisibility('none');
+    }
+    else if(!isContained && visibility==='none'){
+      this.changeVisibility('visible');
+    }
   }
 
   addFeaturesToMap = geojson => {
@@ -128,6 +176,10 @@ class Map extends Component {
             "circle-stroke-width": 1,
             "circle-stroke-color": "#fff"
         }
+    });
+
+    this.map.on('move', e => {
+      this.updateLayerVisibility();
     });
 
     this.map.on('render', 'unclustered-point', e => {
