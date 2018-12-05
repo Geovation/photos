2232c36d181e68cadd0c56fc54e2140901f91be5
Sebastian Ovide <sebastian.ovide@gmail.com>
2018-12-05 15:44:47 +0000

    Theme (#150)
    * removing old css
    
    introducing material UI themes
    
    * packages

--- src/custom/components/AnonymousPage.js
+++ src/custom/components/AnonymousPage.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 
 import Button from '@material-ui/core/Button';
-import backButton from '../images/left-arrow.svg';
+// import backButton from '../images/left-arrow.svg';
 import './AnonymousPage.scss';
 
 class AnonymousPage extends Component {
@@ -15,7 +15,7 @@ class AnonymousPage extends Component {
 
           <Link to="/" style={{ textDecoration: 'none', display: 'block' }}>
             <Button>
-              <img className='buttonback' src={backButton} alt=''/>
+              {/*<img className='buttonback' src={backButton} alt=''/>*/}
             </Button>
           </Link>
 
