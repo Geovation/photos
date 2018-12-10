0defaa7d1aee5b9d03d7646b63caaf1552b6dea3
Sebastian <sebastian.ovide@gmail.com>
2018-12-10 21:38:41 +0000

    externalize firebase config

--- src/custom/config.js
+++ src/custom/config.js
@@ -16,5 +16,13 @@ export default {
   // MAP_SOURCE: "mapbox://styles/mapbox/streets-v10",
   MAP_SOURCE: "https://s3-eu-west-1.amazonaws.com/tiles.os.uk/styles/open-zoomstack-outdoor/style.json",
   MAP_ATTRIBUTION: "Contains OS data &copy; Crown copyright and database rights 2018",
-  MAPBOX_TOKEN: "pk.eyJ1Ijoic2ViYXN0aWFub3ZpZGVnZW92YXRpb251ayIsImEiOiJjanA4ZWwwbTkxdDNxM2twZTgyMGdqOXB5In0.MrWFt3rABCo7n7MBbVRaNw"
+  MAPBOX_TOKEN: "pk.eyJ1Ijoic2ViYXN0aWFub3ZpZGVnZW92YXRpb251ayIsImEiOiJjanA4ZWwwbTkxdDNxM2twZTgyMGdqOXB5In0.MrWFt3rABCo7n7MBbVRaNw",
+  FIREBASE: {
+    apiKey: "AIzaSyBkuZPVStg_zRfUaxLJ-mP4xxdFv8GzdZw",
+    authDomain: "photos-demo-d4b14.firebaseapp.com",
+    databaseURL: "https://photos-demo-d4b14.firebaseio.com",
+    projectId: "photos-demo-d4b14",
+    storageBucket: "photos-demo-d4b14.appspot.com",
+    messagingSenderId: "639308065605"
+  }
 }
--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -7,18 +7,9 @@ import 'firebase/firestore';
 // import 'firebase/functions';
 import 'firebase/storage';
 
-
+import config from './custom/config'
 // Initialize Firebase
-const config = {
-  apiKey: "AIzaSyBkuZPVStg_zRfUaxLJ-mP4xxdFv8GzdZw",
-  authDomain: "photos-demo-d4b14.firebaseapp.com",
-  databaseURL: "https://photos-demo-d4b14.firebaseio.com",
-  projectId: "photos-demo-d4b14",
-  storageBucket: "photos-demo-d4b14.appspot.com",
-  messagingSenderId: "639308065605"
-};
-
-const firebaseApp = firebase.initializeApp(config);
+const firebaseApp = firebase.initializeApp(config.FIREBASE);
 const firestore = firebase.firestore();
 firestore.settings({ timestampsInSnapshots: true });
 
