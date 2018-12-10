e8a69e45c84569e22ec96a4ec0bbf31e9c786542
Sebastian <sebastian.ovide@gmail.com>
2018-12-10 11:09:23 +0000

    removing secondary map as it is too slow

--- src/custom/config.js
+++ src/custom/config.js
@@ -29,5 +29,8 @@ export default {
       secondary: { main: secondaryColor },
     },
   },
+  // MAP_SOURCE: "mapbox://styles/mapbox/streets-v10",
+  MAP_SOURCE: "https://s3-eu-west-1.amazonaws.com/tiles.os.uk/styles/open-zoomstack-outdoor/style.json",
+  MAP_ATTRIBUTION: "Contains OS data &copy; Crown copyright and database rights 2018",
   MAPBOX_TOKEN: "pk.eyJ1Ijoic2ViYXN0aWFub3ZpZGVnZW92YXRpb251ayIsImEiOiJjanA4ZWwwbTkxdDNxM2twZTgyMGdqOXB5In0.MrWFt3rABCo7n7MBbVRaNw"
 }
