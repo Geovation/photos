d3eeeded25a04bb3054f43351a1c42cf4786b534
Konstantinos Dalkafoukis <kdalkafoukis@yahoo.gr>
2018-12-14 15:05:51 +0000

    about page v0.2 (#221)

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,12 +1,38 @@
 import React from 'react';
+import placeholderImage from '../custom/images/geovation_logo.svg';
+
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import './AboutPage.scss';
 
 class AboutPage extends React.Component {
+
+  handleClickButton = () => {
+    this.props.goToPage(this.props.pages.map); // go to the map
+  };
+
   render() {
     return (
-      <div>
-        Hello World About Page!!!
+      <div className={'geovation-about'}>
+        <img className={'logo'} src={placeholderImage} alt='geovation'/>
+        <div className={'main'}>
+          <Typography>
+            We are Geovation and we Geovate
+          </Typography>
+        </div>
+        <div className='button'>
+          <Button
+            fullWidth
+            variant='contained'
+            color='primary'
+            onClick={this.handleClickButton}
+          >
+            Get Collecting
+          </Button>
+        </div>
       </div>
     );
   }
 }
+
 export default AboutPage;
