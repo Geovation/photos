cfe0bbd5825bd3a8f45ad0e85c51f6e7f7a22459
Sebastian <sebastian.ovide@gmail.com>
2019-01-11 12:31:27 +0000

    update scripts

--- scripts/runWithVars.sh
+++ scripts/runWithVars.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+VERSION_MAYOR=$(echo $npm_package_version| cut -d'.' -f 1)
+VERSION_MINOR=$(echo $npm_package_version| cut -d'.' -f 2)
+
 export REACT_APP_BUILD_NUMBER=${TRAVIS_BUILD_NUMBER:-"0123456789"}
-export REACT_APP_VERSION=$npm_package_version
+export REACT_APP_VERSION="$VERSION_MAYOR.$VERSION_MINOR.$REACT_APP_BUILD_NUMBER"
 export REACT_APP_TITLE=$npm_package_title
 
 set -x;
