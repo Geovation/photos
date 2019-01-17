8f87d651853e0ee4893c6c726c7b5cc47a34f9fe
Sebastian <sebastian.ovide@gmail.com>
2019-01-17 10:11:24 +0000

    disabling persistance for iframes

--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -11,15 +11,33 @@ import config from './custom/config'
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(config.FIREBASE);
 const firestore = firebase.firestore();
+
+function isInIframe () {
+  try {
+    return window.self !== window.top;
+  } catch (e) {
+    return true;
+  }
+}
+
 firestore.settings({ timestampsInSnapshots: true });
 
-firestore.enablePersistence()
+// iFrames may break things for security policies. We found it happens at least in safari.
+// more info in https://firebase.google.com/docs/firestore/manage-data/enable-offline
+if (!isInIframe()) {
+  firestore.enablePersistence()
   .catch(function(err) {
     if (err.code === 'failed-precondition') {
       console.error("Multiple tabs open, persistence can only be enabled in one tab at a a time.");
     } else if (err.code === 'unimplemented') {
       console.error("The current browser does not support all of the features required to enable persistence  ...");
+    } else {
+      console.error("Error firestore.enablePersistence(); didn't work" );
     }
   });
+} else {
+  console.log("Cannot enable persistence inside an iframe")
+}
+
 
 export default firebaseApp;
