43546f2afe24cceb7474fce9b9e79eee10de1dad
YousafAzabi <joeazabi@gmail.com>
2019-05-10 11:50:09 +0100

    fix bug validateDOMNesting

--- src/components/EmailVerifiedDialog.js
+++ src/components/EmailVerifiedDialog.js
@@ -5,7 +5,6 @@ import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import withMobileDialog from '@material-ui/core/withMobileDialog';
 import { withStyles } from '@material-ui/core/styles';
@@ -36,24 +35,22 @@ class EmailVerifiedDialog extends React.Component {
         >
           <DialogTitle id="responsive-dialog-title"  style={{textAlign: "center"}}>Email Verification</DialogTitle>
           <DialogContent>
-            <DialogContentText>
-              <p>
-                A verification email has been sent to <b>{user && user.email}</b>.
-                Please click on the link in the email to activate your account.
-              </p>
-              <p>
-                Once you have activated your account, click next to proceed to the app.
-              </p>
-              <p>
-                - If you haven’t received your verification email, check your spam folder or click
-                <b className={classes.link} onClick={authFirebase.sendEmailVerification}> here</b> to resend it.
-              </p>
-              <p>
-                - to carry on without activating your account, click
-                <b className={classes.link} onClick={authFirebase.signOut}> sign out</b>.
-                Some features may not be available until you activate your account.
-              </p>
-            </DialogContentText>
+            <p>
+              A verification email has been sent to <b>{user && user.email}</b>.
+              Please click on the link in the email to activate your account.
+            </p>
+            <p>
+              Once you have activated your account, click next to proceed to the app.
+            </p>
+            <p>
+              - If you haven’t received your verification email, check your spam folder or click
+              <b className={classes.link} onClick={authFirebase.sendEmailVerification}> here</b> to resend it.
+            </p>
+            <p>
+              - to carry on without activating your account, click
+              <b className={classes.link} onClick={authFirebase.signOut}> sign out</b>.
+              Some features may not be available until you activate your account.
+            </p>
           </DialogContent>
           <DialogActions color='secondary'>
             <Button variant='contained' color='secondary' onClick={handleNextClick}>
