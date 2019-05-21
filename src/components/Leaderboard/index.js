b1347efa4684ac27508ebe95c1c35617a36dcff2
YousafAzabi <joeazabi@gmail.com>
2019-05-21 12:51:16 +0100

    Style: highlight 1st row

--- src/components/Leaderboard/index.js
+++ src/components/Leaderboard/index.js
@@ -1,13 +1,11 @@
 import React, {Component} from 'react';
 
-import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
 import StarsIcon from '@material-ui/icons/Stars';
 
 import PageWrapper from '../../components/PageWrapper';
@@ -15,21 +13,26 @@ import config from '../../custom/config';
 
 const styles = theme => ({
   th: {
-    padding: 10,
-    fontSize: 'inherit',
     fontWeight: 'bold',
     color: theme.palette.common.white,
     backgroundColor: 'rgba(0, 0, 0, 0.54)',
   },
   firstRow: {
-    padding: 10,
-    fontSize: 'inherit',
     fontWeight: 'bold',
     color: config.THEME.palette.secondary.main,
   },
-  tb: {
-    padding: 10,
+  cell: {
+    position: 'relative',
+    padding: theme.spacing.unit,
     fontSize: 'inherit',
+  },
+  truncate: {
+    position: 'absolute',
+    top: theme.spacing.unit * 1.5,
+    maxWidth: '90%',
+    whiteSpace: 'nowrap',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
   },
 });
 
@@ -42,32 +45,26 @@ class Leaderboard extends Component {
 
     return (
       <PageWrapper label={label} handleClose={handleClose} hasLogo={false}>
-          <Table >
+          <Table>
 
             <TableHead>
               <TableRow>
-                <TableCell className={classes.th} style={{width:'15%'}}>Rank</TableCell>
-                <TableCell className={classes.th} style={{width:'65%'}}>User</TableCell>
-                <TableCell className={classes.th} style={{width:'20%'}}>Uploaded</TableCell>
+                <TableCell className={`${classes.th} ${classes.cell}`} style={{width:'10%', textAlign:'center'}}>Rank</TableCell>
+                <TableCell className={`${classes.th} ${classes.cell}`} style={{width:'60%'}}>User</TableCell>
+                <TableCell className={`${classes.th} ${classes.cell}`} style={{width:'10%'}}>Uploaded</TableCell>
               </TableRow>
             </TableHead>
 
             <TableBody>
               { usersLeaderboard.map((user, index) => (
-                <TableRow >
-                  <TableCell className={classes.tb} style={{textAlign: 'center'}} component="th" scope="row">
+                <TableRow key={index}>
+                  <TableCell className={classes.cell} style={{textAlign: 'center'}}>
                     {index === 0 ? <StarsIcon color='secondary'/> : index + 1}
                   </TableCell>
-                  {index === 0 ?
-                    <TableCell className={classes.firstRow}>{user.displayName}</TableCell>
-                  :
-                    <TableCell className={classes.tb}>{user.displayName}</TableCell>
-                  }
-                  {index === 0 ?
-                    <TableCell className={classes.firstRow}>{user.uploaded}</TableCell>
-                  :
-                    <TableCell className={classes.tb}>{user.uploaded}</TableCell>
-                  }
+                  <TableCell className={`${!index && classes.firstRow} ${classes.cell}`}>
+                    <div className={classes.truncate}>{user.displayName}</div>
+                  </TableCell>
+                  <TableCell className={`${!index && classes.firstRow} ${classes.cell}`}>{user.uploaded}</TableCell>
                 </TableRow>
               ))}
             </TableBody>
