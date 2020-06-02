e365a569eaec05ce1aa132c89a634bb227837c9b
Sebastian Ovide <sebastian.ovide@geovation.uk>
2020-06-02 13:35:57 +0100

    updated tutorial texts (#530)

--- src/custom/config.js
+++ src/custom/config.js
@@ -22,40 +22,7 @@ import { data } from "./categories";
 const primaryColor = styles.primary;
 const secondaryColor = styles.secondary;
 
-const CUSTOM_STRING = {
-  drawer: {
-    "photos published so far!": "photos published so far!",
-  },
-  tutorial: {
-    "Walk around the city and take photos":
-      "Walk around the city and take photos",
-    "Write info about the photos and upload it to the cloud":
-      "Write info about the photos and upload it to the cloud",
-    "View your images in our interactive map":
-      "View your images in our interactive map",
-  },
-  about: {
-    "We are Geovation and we Geovate":
-      "Backed by years of industry experience and a network that reaches far and wide, we are a community of location-data and proptech collaborators looking to make positive change happen.\n" +
-      "\n" +
-      "Since its inception in 2009 Geovation has become a leading proponent of the value of open innovation in the public sector. After opening its first space in summer 2015, Geovation has grown to support a community of more than 1,200 entrepreneurs, investors, developers, academics, students and corporate innovators.\n" +
-      "\n" +
-      "Our accelerators provide startups up to £20,000 in grant funding, access to data, experienced product development capabilities, geospatial expertise from Ordnance Survey and land and property insight from HM Land Registry, as well as business mentorship and coaching to help prepare for presenting to investors from the wider team and our partners. To date, our accelerator has supported 79 technology start-ups, and we’ve seen nearly £20M raised in investment funding.",
-  },
-  writeFeedback: {
-    "admin@geovation.uk": "it@geovation.uk",
-  },
-  termsAndConditions: {
-    "Welcome to App": "Welcome to PHOTOS",
-    "Please read our ": "Please read our ",
-    "Terms and Conditions": "Terms and Conditions ",
-    " and ": " and ",
-    "Privacy Policy": "Privacy Policy ",
-    "T&C link": " https://geovation.uk/terms-conditions/",
-    "Privacy Policy Link": "https://geovation.uk/privacy-policy/",
-  },
-  tweetMessage: "Check out this #photo ",
-};
+const CUSTOM_STRING = require("./strings.json");
 
 const PAGES = {
   map: {
@@ -145,8 +112,6 @@ export default {
     "Contains OS data &copy; Crown copyright and database rights 2018",
   MAPBOX_TOKEN:
     "pk.eyJ1Ijoic2ViYXN0aWFub3ZpZGVnZW92YXRpb251ayIsImEiOiJjanA4ZWwwbTkxdDNxM2twZTgyMGdqOXB5In0.MrWFt3rABCo7n7MBbVRaNw",
-  GA_TRACKING_ID: "UA-128504979-1",
-  GA_PROPERTY_ID: "TODO: CHANGE IT",
   PHOTO_ZOOMED_FIELDS: {
     updated: (s) => new Date(s).toDateString(),
     description: (s) => s,
