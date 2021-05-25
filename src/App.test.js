e722d126e61fdc6972a6f3a048f1fef1090537f2
Sebastian Ovide <sebastian.ovide@gmail.com>
2021-05-25 14:08:11 +0100

    skiped obsolete tests

--- src/App.test.js
+++ src/App.test.js
@@ -4,7 +4,7 @@ import config from "./custom/config";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders without crashing", () => {
+test.skip("renders without crashing", () => {
   render(
     <HashRouter>
       <App config={config} />
