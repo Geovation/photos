9b9a4bdb783c06282e4634a19b4152f11c2fcaa2
Sebastian Ovide <sebastian.ovide@geovation.uk>
2021-05-28 15:23:06 +0100

    Update node sass and web-vitals (#641)

--- scripts/runWithVars.sh
+++ scripts/runWithVars.sh
@@ -19,15 +19,17 @@ configText=`cat public/config.json`
 
 echo "const config = $configText;" > public/config.js
 
-# any better way ???? it muast be inserted in the HEAD as first thing
-cp node_modules/first-input-delay/dist/first-input-delay.min.js  public/
-
 VERSION_MAYOR=$(echo $npm_package_version| cut -d'.' -f 1)
 VERSION_MINOR=$(echo $npm_package_version| cut -d'.' -f 2)
 
 export REACT_APP_BUILD_NUMBER=${TRAVIS_BUILD_NUMBER:-"0"}
 export REACT_APP_VERSION="$VERSION_MAYOR.$VERSION_MINOR.$REACT_APP_BUILD_NUMBER"
 export REACT_APP_TITLE=$npm_package_title
+
+# TODO:
+# need to copy the pollifill from the library
+#  node_modules/web-vitals/dist/polyfill.js
+# sed  's/<script id="web-vitals">/,/<\/script>/\<\/bla>/g;' public/index.html > public/index.html
 
 eval $@
 set +x;
