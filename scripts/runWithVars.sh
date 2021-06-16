097c886cc41f2fddb1ee35ddbed998483db641ca
Sebastian Ovide <sebastian.ovide@gmail.com>
2021-06-16 14:57:47 +0100

    github variable

--- scripts/runWithVars.sh
+++ scripts/runWithVars.sh
@@ -22,7 +22,7 @@ echo "const config = $configText;" > public/config.js
 VERSION_MAYOR=$(echo $npm_package_version| cut -d'.' -f 1)
 VERSION_MINOR=$(echo $npm_package_version| cut -d'.' -f 2)
 
-export REACT_APP_BUILD_NUMBER=${TRAVIS_BUILD_NUMBER:-"0"}
+export REACT_APP_BUILD_NUMBER=${GITHUB_RUN_NUMBER:-"0"}
 export REACT_APP_VERSION="$VERSION_MAYOR.$VERSION_MINOR.$REACT_APP_BUILD_NUMBER"
 export REACT_APP_TITLE=$npm_package_title
 
