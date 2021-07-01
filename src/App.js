01191af74f4984718b6aa0e2f2c27e3798f5290b
Sebastian Ovide <sebastian.ovide@gmail.com>
2021-07-01 15:55:40 +0100

    removed promise props

--- src/App.js
+++ src/App.js
@@ -85,7 +85,6 @@ const App = (props) => {
   const [dialogTitle, setDialogTitle] = useState("");
   const [dialogContentText, setDialogContentText] = useState("");
   const [confirmDialogTitle, setConfirmDialogTitle] = useState("");
-  const [newVersionAvailable, setNewVersionAvailable] = useState(false);
   const [ignoreUpdate, setIgnoreUpdate] = useState(false);
 
   const geolocationContext = useContext(GeolocationContext);
@@ -756,7 +755,7 @@ const App = (props) => {
       </main>
 
       <Snackbar
-        open={newVersionAvailable && !ignoreUpdate}
+        open={props.newVersionAvailable && !ignoreUpdate}
         key="topcenter"
         anchorOrigin={{ vertical: "top", horizontal: "center" }}
       >
